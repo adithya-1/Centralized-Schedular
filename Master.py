6aab4bee0bd24cb531098e7fddb905abc0b83511
adithya-1 <adithyaaravi10@gmail.com>
2020-12-02 23:11:28 +0530

    commenting done: found error where it fails for two digit job ids

--- Master.py
+++ Master.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+
+#importing required libraries
 import sys
 import json
 import socket
@@ -7,41 +9,49 @@ import time
 import threading
 import datetime
 
+#initialising locks
 lock1=threading.Lock()
 lock2=threading.Lock()
 lock3=threading.Lock()
 lock4=threading.Lock()
+
+#getting arguments from user
 pathToConfig= sys.argv[1]
 schType=sys.argv[2]
 
+#definition of worker class
 class Worker:
     def __init__(self,portNo,workerId,noSlots):
-        self.portNo=portNo
+        #port no
+        self.portNo=portNo 
+        #workerId
         self.workerId=workerId
+        #total number of slots
         self.noSlots=noSlots
+        #total number of available slots
         self.avaSlots=noSlots
+        #jobs running in each slot
         self.slotJobs=dict()
+        #initialising slot jobs
         for i in range(1,self.noSlots+1):
-            self.slotJobs[i]=[True,0,'']
-        
-        
+            self.slotJobs[i]=[True,0,'']   #[slotAvailable,duration value,running task]
 
-    
-    def updateAva(self,ava):
-        self.avaSlots=ava
-
+#loading configuration file
 with open(pathToConfig) as f:
         data = json.load(f)
-
+#getting list of workers
 workers=data['workers']
+
+#global workers dictionary
 globalWorkers=dict()
 
-
+#initialising global dictionary with portNo,workerId,Slots
 for i in workers:
     globalWorkers[i['worker_id']]=Worker(i['port'],i['worker_id'],i['slots'])
 
+#Random Scheduling Algorithm - gets a random woker and checks if it has avaliable slots,if slot exsist then return the worker else look randomly
+#for another worker
 def schedulingRandom():
-  
     solWorker=random.choice(list(globalWorkers.values()))
     while True:
         
@@ -50,17 +60,16 @@ def schedulingRandom():
         else:
             solWorker=random.choice(list(globalWorkers.values()))
 
+#Round Robin Scheduling Algorithm - sorts the workers based on id and checks if slots are avaiable accordingly
 def schedulingRound():
-   
     while True:
-   
-        
         temp=sorted (globalWorkers.keys())
         for k in temp:
         
             if(globalWorkers[k].avaSlots>0):
                 return globalWorkers[k]
 
+#Least- Loaded Scheduling Algorithm - checks for the worker with most available slots.If no slots are available then waits for one second before checking again
 def schedulingLeast():
     temp=list(globalWorkers.values())
     while True:
@@ -75,56 +84,84 @@ def schedulingLeast():
             time.sleep(1)
         else:
             return solWorker
+
+#To check list of jobs to be processed
 globalJobContent=dict()
 
+#Class TCP Server
 class TCPServer:
     def __init__(self,port):
+        #port no
         self.port = port
+        #ip 
         self.ip = "localhost"
+        #socket connection
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #bind to respective id and port
         self.sock.bind((self.ip, self.port))
+        #listen to the port for incoming requests
         self.sock.listen(1)
+        #queue of jobs for the server
         self.jobQueue=[]
        
-
+    #definition to start the server
     def startserver(self):
+        
+        #keep the server running
         while True:
                 print('waiting for a connection at ',self.port)
+                #accept incoming requests
                 connection, clientAddress = self.sock.accept()
+                #receive the job
                 job=connection.recv(1024)
+                #append to job queue so that incoming jobs are not delayed
                 self.jobQueue.append(job)
                 print(f'port {self.port} receives {job} from {clientAddress}')
-                connection.close()      
+                #close the connection
+                connection.close() 
+                #decode the message     
                 analyticMessage=job.decode('utf-8')
+                #convert decoded message to dictionary
                 analyticMessage=json.loads(analyticMessage)
                
+               #logging recevied jobs
                 try:
                     fileWrite="received:"+str(analyticMessage['job_id'])+","+str(datetime.datetime.now().timestamp() * 1000)+"\n"
                     f.write(fileWrite)
                     
                 except:
+                    #the last request will be empty
                     print('')
                
-                     
+                 
+                #poping the first item in job queue    
                 individualJob=self.jobQueue.pop(0)
                 
                 
+                #function to send jobs to respective workers
                 if(self.port==5000):
                     
+                    #decode the message
                     message=individualJob.decode('utf-8') 
+                    #convert decoded message to dictionary
                     message=json.loads(message)
+                    #storing jobs in global job list 
                     globalJobContent[message['job_id']]=list()
+                    #first appending map tasks
                     subList=list()
                     for i in message['map_tasks']:
                         subList.append([i['task_id'],i['duration'],False])
                     globalJobContent[message['job_id']].append(subList)
+                    #second appending reducer tasks
                     subList=list()
                     for i in message['reduce_tasks']:
                         subList.append([i['task_id'],i['duration'],False])
                     globalJobContent[message['job_id']].append(subList)
                     
+                    #boolean value to that signifies whether to start reducer or not
                     globalJobContent[message['job_id']].append(False)
                     
+                    #finding worker based on scheduling alogrithm for map tasks
                     for i in globalJobContent[message['job_id']][0]:
                         tempWorker=None
                         if(schType=='RANDOM'):
@@ -134,27 +171,36 @@ class TCPServer:
                         elif(schType=='LL'):
                             tempWorker=schedulingLeast()
                         
-                        # tempWorker=globalWorkers[1]
-                        
+                       
+                        #after the worker is returned based on scheduling algorithm sending request to that worker
                         if(tempWorker!=None):
-                        
                             send_request(i,tempWorker)
-                        
+                
+                #function to receive message from workers        
                 if(self.port==5001):
+                    #decode the message
                     message=individualJob.decode('utf-8') 
+                    #convert decoded message to dictionary
                     message=json.loads(message)
+                    #acquring locks to change available slots and slot jobs 
                     lock2.acquire()
                     globalWorkers[message['worker_id']].avaSlots=message['avaSlots']
                     globalWorkers[message['worker_id']].slotJobs[message['slot_id']]=message['slotJobs']
+                    #releasing the jobs
                     lock2.release()
+                    
+                    #seeing whch job got completed
                     a=message['jobCompleted']
+                    #if the job is a mapper task
                     if(a[2]=='M'):
-                      
+                        
+                        #updating that mapper task is completed in global job list
                         for i in globalJobContent[a[0]][0]:
                             if(i[0]==message['jobCompleted']):
                                 i[2]=True
                                 break
-                            
+                        
+                        #checking to see if reducer fnction can start or not   
                         startReducer=False
                         for i in globalJobContent[a[0]][0]:
                             if(i[2]==False):
@@ -162,15 +208,24 @@ class TCPServer:
                                 break
                             else:
                                 startReducer=True
+                                
                         print(startReducer)
+                        #if reducer to be started
                         if(startReducer):
+                            #inidicating reducer functions have been started in global job list
                             globalJobContent[a[0]][2]=True
                         for k,v in globalJobContent.items():
+                            #if for that particular job reducer is started
                             if(v[2]):
+                                #if there are reducer tasks pending
                                 if(len(v[1])>0):
+                                    #acquire lock to get which reducer function to be sent
                                     lock3.acquire()
+                                    #poping the first reducer in the list
                                     reducerJob=v[1].pop(0)
+                                    #releasing the lock 
                                     lock3.release()
+                                    #finding worker based on scheduling alogrithm for reduce tasks
                                     tempWorker=None
                                     if(schType=='RANDOM'):
                                         tempWorker=schedulingRandom()
@@ -180,27 +235,38 @@ class TCPServer:
                                         tempWorker=schedulingLeast()
                                     
                                     if(tempWorker!=None):
+                                        #send the request
                                         send_request(reducerJob,tempWorker)
+                                    #send only ne reduce task per loop
                                     break
 
                                     
-                    
+                    #if message is a reducer
                     elif(a[2]=='R'):
+                        #updating global job list
                         for i in globalJobContent[a[0]][1]:
                             if(i[0]==message['jobCompleted']):
                                 i[2]=True
                                 break
+                            
                         for k,v in globalJobContent.items():
+                            #if for that particular job reducer is started
                             if(v[2]):
+                                #if all the reduce task have been completed
                                 if(len(v[1])==0):
                                     fileWrite="completed:"+str(a[0])+","+str(datetime.datetime.now().timestamp() * 1000)+"\n"
                                     f.write(fileWrite)
                                     
                                     break
+                                #if there are reducer tasks pending
                                 if(len(v[1])>0):
+                                    #acquire lock to get which reducer function to be sent
                                     lock4.acquire()
+                                    #poping the first reducer in the list
                                     reducerJob=v[1].pop(0)
+                                    #releasing the lock 
                                     lock4.release()
+                                    #finding worker based on scheduling alogrithm for reduce tasks
                                     tempWorker=None
                                     if(schType=='RANDOM'):
                                         tempWorker=schedulingRandom()
@@ -212,28 +278,30 @@ class TCPServer:
                                     if(tempWorker!=None):
                                         send_request(reducerJob,tempWorker)
                                     break
-                            # if(len(v[1])==0):
-                            #         fileWrite="completed:"+str(a[0])+","+str(datetime.datetime.now().timestamp() * 1000)+"\n"
-                            #         f.write(fileWrite)
                         
                     else:
-                        print('Error in 200')
+                        #used for debugging purpose
+                        print('Error in line 284')
                                
                     
                    
                   
                     
                     
-                              
+#initialising TCP server at 5000                             
 s5000=TCPServer(5000)
+#initialising TCP server at 5001
 s5001=TCPServer(5001)
 
-
+#send request function
 def send_request(job,worker):
+    #acquring locks to change number of availabe slots
     lock1.acquire()
     worker.avaSlots-=1
+    #changing the message as per requirement
     x=job
     job={"task_id":x[0],"duration":x[1]}
+    #updating the slots in each worker
     for k,v in worker.slotJobs.items():
         if(v[0]):
             v[0]=False
@@ -242,6 +310,7 @@ def send_request(job,worker):
             break
     lock1.release()
     
+    #sending the message to the specified worker
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect(("localhost", worker.portNo))
             
@@ -249,11 +318,14 @@ def send_request(job,worker):
             
         print(f'sending {message} to {worker.portNo}')   
         s.send(message.encode())
-        
+
+#opening log files to record job initial request time and job completion time        
 f=open("masterJobLogs.txt","a+",buffering=1)
+#log file tasks for each scheduling algorithm
 fileOneName=str(schType)+"Logs.txt"
 f1=open(fileOneName,"a+",buffering=1)
 
+#checking total number of jobs running on each machine for every second
 def analysisTwo():
     timeX=0
     while(True):
@@ -264,7 +336,7 @@ def analysisTwo():
         timeX+=1
         time.sleep(1)
     
-
+#initiating threading for parallelism
 threads = [threading.Thread(target=s5000.startserver), threading.Thread(target=s5001.startserver),threading.Thread(target=analysisTwo)]
 
 
--- Worker.py
+++ Worker.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+#Importing neccessary libraries for computation
 import sys
 import json
 import socket
@@ -6,95 +7,108 @@ import threading
 import time
 import datetime
 
+#Getting the required port number and worker ID as arguments from command line
 portNo=int(sys.argv[1])
 workerId=int(sys.argv[2])
 
+#Initializing locks
 lock1=threading.Lock()
 lock2=threading.Lock()
 lock3=threading.Lock()
 
+
+#Creating a class for Worker
 class Worker:
-    def __init__(self,portNo,workerId,noSlots):
+    def __init__(self,portNo,workerId,noSlots):     #Initializing parameters for Worker object
         self.portNo=portNo
         self.workerId=workerId
         self.noSlots=noSlots
         self.avaSolts=noSlots
         self.slotJobs=dict()
-        for i in range(1,self.noSlots+1):
+        for i in range(1,self.noSlots+1):        #First Bool value specifies available slots, second parameter indicates duration and third parameter indicates task_id
             self.slotJobs[i]=[True,0,'']
             
         
         
     def useSlot(self):
-        
+        #Throw error if there are no slots available for the worker
         if(self.avaSolts==0):
             print('Error in 26')
-        else:
+        else:      # Subtract a slot when worker is alloted tasks
             self.avaSolts-=self.avaSolts
             
     
     def releaseSlot(self):
-        if(not (self.avaSolts>=self.noSlots)):
+        if(not (self.avaSolts>=self.noSlots)):      # Once task is done executing, slot is made free again
             self.avaSolts+=1
         else:
-            print('Error in 35')
-         
+            print('Error in 35')     #Throw an error if slot numbers exceeds allocated slots for the worker
+
+
+#Loading data from the config file
 with open('config.json') as f:
             data = json.load(f)
 
+#Setting up TCP connection
 class TCPServer:
+#Initializing paramters for TCP socket
     def __init__(self,port):
         self.port = port
         self.ip = "localhost"
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.jobQueue=[]
+        self.jobQueue=[]      #Creating a queue for received jobs
 
+#Starting the TCP server
     def startserver(self):
         self.sock.bind((self.ip, self.port))
         self.sock.listen(1)
-        while True:
+        while True:    #Listening to Master TCP to send job data
                 print('waiting for a connection at ',self.port)
                 connection, clientAddress = self.sock.accept()
-                job=connection.recv(1024)
-                self.jobQueue.append(job)
+                job=connection.recv(1024)     #Getting job data from TCP socket
+                self.jobQueue.append(job)      #Appending the received job into the job queue
                 connection.close()
-                print(f'port {self.port} receives {job}')
+                print(f'port {self.port} receives {job}')      #Displaying job reveceived
                 individualJob=self.jobQueue.pop(0)
-                
+                #Accessing job parameters from job data by loading into json
                 message=individualJob.decode('utf-8')
                 message=json.loads(message)
-                lock1.acquire()
-                workerClass.avaSolts-=1
-                for k,v in workerClass.slotJobs.items():
+                lock1.acquire()      #Consume a lock for the job
+                workerClass.avaSolts-=1     #Consume a slot for the job
+                for k,v in workerClass.slotJobs.items():      #Changing slot parameters of the worker when job gets assigned
                     if(v[0]):
-                        v[0]=False
+                        v[0]=False      #Changing slot status for be not free
                         v[1]= int(message["duration"])
                         v[2]=message["task_id"] 
                         break  
                         
-                lock1.release() 
+                lock1.release()     #Release lock after initiation of job
+                
+                #Send the time since epoch for received task into a file for logs analysis
                 fileWrite="received:"+str(message['task_id'])+","+str(datetime.datetime.now().timestamp() * 1000)+"\n"
                 print(fileWrite)
                 f.write(fileWrite)  
               
                     
-
+# Sending a message back to Master using 5001
 def send_request():
     
     while(True):
-        
+        #Changing slot variabels on completion of task
         for k,v in workerClass.slotJobs.items():
-            
+            #If duration is 0 i,e when task is completed
             if(v[1]==0 and v[2]!=''):
-                jobCompleted=v[2]
-                lock2.acquire()
-                workerClass.avaSolts+=1
+                jobCompleted=v[2]    #Add task to completed task list
+                lock2.acquire()      #Consume another lock
+                workerClass.avaSolts+=1      #Free the slot for the worker
                 v[2]=''
-                v[0]=True
+                v[0]=True            #Set slot status as available
                  
-                lock2.release()
+                lock2.release()     #Release the lock
+                #Append the time since epoch when the task is completed into a file for logs analysis
                 fileWrite="completed:"+str(jobCompleted)+","+str(datetime.datetime.now().timestamp() * 1000)+"\n"
-                f.write(fileWrite) 
+                f.write(fileWrite)
+                #Send the above information to Master via TCP socket through port 5001
                 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                     s.connect(("localhost", 5001))
                     message={"worker_id":workerClass.workerId,"avaSlots":workerClass.avaSolts,"slotJobs":v,"slot_id":k,"jobCompleted":jobCompleted}
@@ -102,15 +116,19 @@ def send_request():
                     print(f'sending {message} to {5001}') 
                     message=json.dumps(message)
                     
-                    #send task
+                    #sending the task
                     s.send(message.encode())
-            elif(v[1]>0):
+                    
+            elif(v[1]>0):      #If task is not completed, then sleep for 1 second and decrease the duration for the task
                 
                 lock3.acquire()
                 v[1]-=1
                 lock3.release()
         
         time.sleep(1)
+        
+        
+#Checking for matching parameters specified for workers using config file
 workers=data['workers']
 workerClass=None
 for i in range(0,len(workers)):
@@ -119,11 +137,11 @@ for i in range(0,len(workers)):
         workerClass=Worker(portNo,workerId,workers[i]['slots'])
         break
 
-
+#If parameters not matched, error is thrown
 if(workerClass==None):
     print('Sorry given worker id  does not match with respective port number.Please fill appropriate inputs to the program')
     exit
-else:
+else:    #Closing the file which was creating for logs analysis
     fileName='workerLogs'+str(workerClass.portNo)+'.txt'
     
     
@@ -131,6 +149,7 @@ else:
     
     serverWorker=TCPServer(workerClass.portNo)
 
+#initiating threading for parallelism
     threads = [threading.Thread(target=serverWorker.startserver), threading.Thread(target=send_request)]
     
 
